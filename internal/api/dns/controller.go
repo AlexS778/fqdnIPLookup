e07c00beb57bc1b945c07ae360d52164f3704ea8
AlexS778 <aleksej.serbionov29@gmail.com>
2023-08-09 10:27:47 +0300

    changed endpoints url's to non register specific

--- internal/api/dns/controller.go
+++ internal/api/dns/controller.go
@@ -14,8 +14,8 @@ func RegiserV1Routes(router *gin.Engine, db *sql.DB) {
 	h := &handler{DB: db}
 	v1 := router.Group("/v1")
 	{
-		v1.POST("/FQDNToIP", h.FQDNToIPHanlder)
-		v1.POST("/IPToFQDN", h.IPToFQDN)
+		v1.POST("/fqdntoip", h.FQDNToIPHanlder)
+		v1.POST("/iptofqdn", h.IPToFQDN)
 		v1.POST("/whoishere", h.GetWhoIsData)
 	}
 }
